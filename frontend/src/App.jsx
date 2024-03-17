c38307def8aca66f174f28e6efabc58c2754194b
devansh <devansh31shah@gmail.com>
2024-03-17 21:10:15 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import "./app.css";
+import Layout from "./layout/layout";
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return <Layout></Layout>
+   
+  
+}
+
+export default App
